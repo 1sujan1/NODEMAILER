d4fce3b724c5f4f430e8ef7a83cd32a35c6f6f56
Sujan Lamichhane <suzon9849@gmail.com>
2025-02-21 12:39:58 +0545

    change file path

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,18 +1,30 @@
 const express = require("express");
-const customerController = require("../controllers/customerController");
+const customerModel = require("../models/customerModel");
 
 const router = express.Router();
 
 // GET all customers
-router.get("/", customerController.getAllCustomers);
+router.get("/", (req, res) => {
+  const customers = customerModel.getAllCustomers();
+  res.json(customers);
+});
 
 // POST a new customer
-router.post("/", customerController.addCustomer);
-
-// PUT update a customer
-router.put("/:id", customerController.updateCustomer);
+router.post("/", (req, res) => {
+  const newCustomer = req.body;
+  customerModel.addCustomer(newCustomer);
+  res.status(201).json(newCustomer);
+});
 
 // DELETE a customer
-router.delete("/:id", customerController.deleteCustomer);
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const success = customerModel.deleteCustomer(id);
+  if (success) {
+    res.status(204).send();
+  } else {
+    res.status(404).send("Customer not found");
+  }
+});
 
 module.exports = router;
